390badd455d8d74227152e9b1be59225283a4e59
Lolztech <123384950+Lolztech@users.noreply.github.com>
2025-01-04 00:44:09 +0000

    Small Fixes + Dynamic Sensitivity Settings in Pause Menu
    Stopped PlayerCam rotation while paused
    
    Implemented functioning sensitivity settings in pause menu that can be changed on the go

--- Assets/Scripts/Scene Management/PuaseManager.cs
+++ Assets/Scripts/Scene Management/PuaseManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject SettingsPanel;
     [SerializeField] private GameObject MainMenuConfirmationPanel;
     [SerializeField] private GameObject PlayerUI; // Reference to the player UI
+
+    [SerializeField] private Slider sensXSlider; // Slider for sensitivity X
+    [SerializeField] private Slider sensYSlider; // Slider for sensitivity Y
 
 
     private bool isPaused = false;
@@ -30,12 +34,18 @@ public class PauseMenu : MonoBehaviour
         SettingsPanel.SetActive(false);
         MainMenuConfirmationPanel.SetActive(false);
         PlayerUI.SetActive(true); // Re-enable player UI
-        
 
         Time.timeScale = 1f; // Unpause the game
         Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the game
         Cursor.visible = false; // Hide the cursor
         isPaused = false;
+
+        // Notify PlayerCam
+        PlayerCam playerCam = Object.FindFirstObjectByType<PlayerCam>();
+        if (playerCam != null)
+        {
+            playerCam.isPaused = false;
+        }
     }
 
     public void Pause()
@@ -46,13 +56,19 @@ public class PauseMenu : MonoBehaviour
         MainMenuConfirmationPanel.SetActive(false);
         PlayerUI.SetActive(false); // Disable player UI
 
-
         Time.timeScale = 0f;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
         Canvas.ForceUpdateCanvases(); // Recalculate UI
         isPaused = true;
+
+        // Notify PlayerCam
+        PlayerCam playerCam = Object.FindFirstObjectByType<PlayerCam>();
+        if (playerCam != null)
+        {
+            playerCam.isPaused = true;
+        }
     }
 
     public void Back()
@@ -98,4 +114,27 @@ public class PauseMenu : MonoBehaviour
         MainPanel.SetActive(true);
         MainMenuConfirmationPanel.SetActive(false);
     }
+
+    public void SaveSettings()
+    {
+        if (GeneralSettings.Instance != null)
+        {
+            GeneralSettings.Instance.sensX = sensXSlider.value;
+            GeneralSettings.Instance.sensY = sensYSlider.value;
+
+            Debug.Log($"Saved Sensitivity - X: {sensXSlider.value}, Y: {sensYSlider.value}");
+
+            // Notify PlayerCam of the updated settings
+            PlayerCam playerCam = Object.FindFirstObjectByType<PlayerCam>(); // Updated to use the new method
+            if (playerCam != null)
+            {
+                playerCam.UpdateSensitivity(GeneralSettings.Instance.sensX, GeneralSettings.Instance.sensY);
+            }
+            else
+            {
+                Debug.LogWarning("PlayerCam not found!");
+            }
+        }
+    }
+
 }
